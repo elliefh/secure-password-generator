4c6e2a891a5bdc69194001bea3af3ffd25595f53
elliefh <elliefu18@gmail.com>
2020-09-11 09:01:08 -0700

    cleaned up code, deleted readme comments

--- script.js
+++ script.js
@@ -70,32 +70,6 @@ function generatePassword() {
   }
 }
 
-
-// function that generates secure password based on user's criteria
-// function newPassword {
-//   for (let i = 0; index < passwordLength; i++) {
-//     newPassword[i]=(numeric[Math.floor(Math.random()*10)]);
-//     // uppercase[Math.floor(Math.random()*26)];
-//     // lowercase[Math.floor(Math.random()*26)];
-//     // special[Math.floor(Math.random()*26)];
-//   }
-// }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
